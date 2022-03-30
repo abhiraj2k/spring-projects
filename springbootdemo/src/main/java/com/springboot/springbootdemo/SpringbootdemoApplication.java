2a0e44c7c7e8d0f21f0e2775338ac3dc683730b6
Abhishek Bhavsar <abhiraj2k@gmail.com>
2022-03-30 14:30:49 +0530

    ADDED: SpringBoot Application

---
+++ springbootdemo/src/main/java/com/springboot/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,18 @@
+package com.springboot.springbootdemo;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoApplication.class, args);
+	}
+
+}
---
+++ springbootdemo/src/main/java/com/springboot/springbootdemo/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.springboot.springbootdemo.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("/")
+	public String welcome() {
+		return "<h1>Hello visitor !!</h1>";
+	}
+	@GetMapping("/task")
+	public String task(){
+		return "Task demonstration";
+	}
+	@GetMapping("/task/showMyInfo")
+	public Map<String,String> showMyInfo() {
+		return Map.of("name","Abhishek","city","Pune","state","Maharashtra","dream_job","Software Developer");
+	}
+	@GetMapping("task/showMyHobbies")
+	public List<String> showMyHobbies(){
+		return List.of("MMA","Gaming","Riding Bike");
+	}
+}
